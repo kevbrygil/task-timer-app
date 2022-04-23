c7306d3841737c4ec0baf6c26e8d3fcf76037c11
Gilbert Tamayo <tamayo_144@hotmail.com>
2022-04-23 14:35:14 -0500

    anexo del formulario de registro, tambien se modificaron las configuraciones de jsconfig

---
+++ src/components/signup-page/SignupForm.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { LabelAndInput } from '../Form'
+import Button from '../Button'
+import { postFormData } from '../../utils/postFormData'
+
+const SignupForm: React.FC = () => {
+    const [username, setUsername] = useState('')
+    const [password1, setPassword1] = useState('')
+    const [password2, setPassword2] = useState('')
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        const formData = { username, password1, password2 }
+        const { response, result } = await postFormData(formData, '/api/signup')
+        console.log({ response, result })
+    }
+
+    return (
+        <form className="w-full sm:w-96" onSubmit={handleSubmit}>
+            <LabelAndInput
+                label="Username"
+                inputType="text"
+                inputName="username"
+                handleChange={(e) => setUsername(e.target.value)}
+                value={username}
+            />
+            <LabelAndInput
+                label="Password"
+                inputType="password"
+                inputName="password1"
+                handleChange={(e) => setPassword1(e.target.value)}
+                value={password1}
+            />
+            <LabelAndInput
+                label="Confirm password"
+                inputType="password"
+                inputName="password2"
+                handleChange={(e) => setPassword2(e.target.value)}
+                value={password2}
+            />
+            <Button color="success" type="submit" extraClasses="w-full mt-3 py-3 font-semibold">
+                Create Account
+            </Button>
+        </form>
+    )
+}
+
+export default SignupForm
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from "next"
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1 className="text-red-600">Hello World</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1 className="text-red-600">Hello World</h1>
+        </div>
+    )
 }
 
 export default Home
